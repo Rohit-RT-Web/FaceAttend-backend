e61ebc5ffde87951f6834e7126c8b0a338005ba6
Rohit Saini <rohitsaini24621@gmail.com>
2026-06-02 11:14:25 +0530

    Updated library management frontend

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/attendance_system";
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
 // Middleware
 app.use(cors());
