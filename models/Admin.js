00a3840c196bf5ba34eb8257fd85e761939a76b1
Rohit Saini <rohitsaini24621@gmail.com>
2026-06-04 16:56:55 +0530

    Updated library management frontend

--- models/Admin.js
+++ models/Admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const adminSchema = new mongoose.Schema({
   email: {
@@ -18,9 +19,16 @@ const adminSchema = new mongoose.Schema({
   },
 });
 
-// Password direct compare karo (plain text)
+// Password save karne se pehle hash karo
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Password compare karo
 adminSchema.methods.comparePassword = async function (plainPassword) {
-  return this.password === plainPassword;
+  return bcrypt.compare(plainPassword, this.password);
 };
 
 module.exports = mongoose.model("Admin", adminSchema);
